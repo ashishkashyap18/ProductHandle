0528b2ffa5409e3b958d40605e7a5088b9c7de63
ashishkashyap18 <ashish.avinya@gmail.com>
2024-09-11 12:24:10 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductHandle.Models;
+
+namespace ProductHandle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly WebApiContext _webApiContext;
+        public ProductController(WebApiContext webApiContext)
+        {
+            _webApiContext = webApiContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _webApiContext.Products.ToListAsync();
+        }
+
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var product = await _webApiContext.Products.FindAsync(id);
+            return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Product product)
+        {
+            if (id != product.ProductId) return BadRequest();
+            _webApiContext.Entry(product).State = EntityState.Modified;
+            await _webApiContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Created(Product product)
+        {
+            await _webApiContext.Products.AddAsync(product);
+            await _webApiContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProductById), new {id=product.ProductId},product);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productToDelete = await _webApiContext.Products.FindAsync(id);
+            if(productToDelete == null) return NotFound();
+            _webApiContext.Products.Remove(productToDelete);
+            await _webApiContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Models/WebApiContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductHandle.Models;
+
+public partial class WebApiContext : DbContext
+{
+    public WebApiContext()
+    {
+    }
+
+    public WebApiContext(DbContextOptions<WebApiContext> options)
+        : base(options)
+    {
+    }
+
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-RT2C6UP;Database=WebApi;Trusted_Connection=True; Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6CDE81ECF49");
+
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.SupplierName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Weight).HasColumnType("decimal(18, 0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
